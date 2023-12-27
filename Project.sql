c3df4b23d8ff55e0b73c37ec61960ccca55eb0e4
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2023-12-27 09:10:43 +0700

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -37,3 +37,7 @@ EndDate DATETIME,
 Reriod INT,
 Cost MONEY,
 )
+
+INSERT INTO Project VALUES (001, 'Lab1', '2023-1-3', '2023-6-6', 5, 400)
+INSERT INTO Project VALUES (002, 'Lab2', '2023-2-4', '2023-9-14', 7, 600)
+INSERT INTO Project VALUES (003, 'Lab3', '2023-3-5', '2023-7-20', 4, 350 )
