8a3426702f02a1a25ae4ff516093a188ca1e228e
VanNgo1610 <vanthngo.csp@gmail.com>
2023-12-27 09:06:09 +0700

    Create Project.sql

---
+++ Project.sql
@@ -0,0 +1,39 @@
+﻿--Tạo bảng Employee
+CREATE TABLE Employee(
+EmployeeID INT PRIMARY KEY,
+EmployeeName NVARCHAR(100),
+Telephone CHAR(11),
+EmailAddress VARCHAR(30),
+)
+
+INSERT INTO Employee VALUES (1, 'Ngo', '0123456789', 'vanthngo.csp@gmail.com')
+INSERT INTO Employee VALUES (2, 'Thuy', '0123456789', 'vanthngo.csp@gmail.com')
+INSERT INTO Employee VALUES (3, 'Van', '0123456789', 'vanthngo.csp@gmail.com')
+
+DROP TABLE Employee;
+
+SELECT * FROM Employee;
+
+CREATE TABLE EmployeeGroup(
+GroupID INT PRIMARY KEY,
+GroupName VARCHAR(30),
+LeaderID INT,
+ProjectID INT,
+)
+
+INSERT INTO EmployeeGroup VALUES (01, 'FPT', 2, 987)
+INSERT INTO EmployeeGroup VALUES (02, 'Aptech', 3, 876)
+INSERT INTO EmployeeGroup VALUES (03, 'FPTA', 1, 765)
+
+DROP TABLE EmployeeGroup;
+
+SELECT * FROM EmployeeGroup;
+
+CREATE TABLE Project(
+ProjectID INT PRIMARY KEY,
+ProjectName VARCHAR(30),
+StartDate DATETIME,
+EndDate DATETIME,
+Reriod INT,
+Cost MONEY,
+)
