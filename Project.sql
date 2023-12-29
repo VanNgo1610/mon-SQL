67166ef3fae2eb1de19352977295913da227b27b
VanNgo1610 <142959206+VanNgo1610@users.noreply.github.com>
2023-12-29 08:00:13 +0700

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -41,3 +41,41 @@ Cost MONEY,
 INSERT INTO Project VALUES (001, 'Lab1', '2023-1-3', '2023-6-6', 5, 400)
 INSERT INTO Project VALUES (002, 'Lab2', '2023-2-4', '2023-9-14', 7, 600)
 INSERT INTO Project VALUES (003, 'Lab3', '2023-3-5', '2023-7-20', 4, 350 )
+
+CREATE TABLE GroupDetail(
+	GroupID  INT,
+	EmployeeID INT ,
+	Status CHAR(20),
+	PRIMARY KEY (GroupID, EmployeeID),
+	FOREIGN KEY (GroupID ) REFERENCES  Groups (GroupID ),
+	FOREIGN KEY (EmployeeID) REFERENCES Employee (EmployeeID)
+);
+
+INSERT INTO GroupDetail VALUES (123, 1, 'hoan thanh'),
+INSERT INTO GroupDetail VALUES (234, 2, 'hoan thanh'),
+INSERT INTO GroupDetail VALUES (345, 3, 'hoan thanh');
+
+
+SELECT * FROM Employee;
+SELECT * FROM Groups;
+SELECT * FROM Project;
+SELECT * FROM GroupDetail;
+
+SELECT E.*  FROM Employee AS E, Project AS P, Groups AS G, GroupDetail AS GD
+WHERE GD.GroupID = G.GroupID
+AND E.EmployeeID = GD.EmployeeID
+AND G.ProjectID = P.ProjectID
+AND P.ProjectName = 'msa'
+
+
+SELECT G.GroupName, COUNT(gd.EmployeeID) AS EmployeeCount
+FROM Groups G
+LEFT JOIN GroupDetail gd ON G.GroupID = GD.GroupID
+GROUP BY G.GroupName
+
+SELECT E.* FROM Employee E
+JOIN Groups G ON E.EmployeeID = G.LeaderID;
+
+
+ALTER TABLE Employee
+ALTER COLUMN Name NVARCHAR(100) NOT NULL;
